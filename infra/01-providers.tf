73f934b0c0dc5e7e667f122f5b99005ac469e29f
pibabu <153180115+pibabu@users.noreply.github.com>
2025-11-03 16:23:10 +0100

    no tf module

--- infra/01-providers.tf
+++ infra/01-providers.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region  = "eu-central-2"
 }
 
+
 terraform {
   required_version = ">= 1.0"
 
--- infra/02-vpc.tf
+++ infra/02-vpc.tf
@@ -49,3 +49,6 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+
+#lieber module?
