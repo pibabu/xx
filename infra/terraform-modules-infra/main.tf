5329ddaf52fc744fa7e111897f9dab9dbd6fbf79
pibabu <153180115+pibabu@users.noreply.github.com>
2025-11-16 16:05:49 +0100

    deploy ohne ebs

--- infra/terraform-modules-infra/main.tf
+++ infra/terraform-modules-infra/main.tf
@@ -47,7 +47,7 @@ module "ec2_instance_module" {
 # }
 
 
-#  !!
+#  !! 
 # resource "aws_eip_association" "eip_assoc" {
 #   instance_id   = module.ec2_instance_module.instance_id
 #   allocation_id = data.terraform_remote_state.persistent.outputs.eip_allocation_id
